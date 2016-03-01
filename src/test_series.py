ae81d91ff82a1d9eb91b2843cf6d6f992d6cd071
Norton Pengra <npengra317@gmail.com>
2016-03-01 14:01:22 -0800

    Added Lucas function and test.

--- src/test_series.py
+++ src/test_series.py
@@ -13,9 +13,28 @@ FIBB_TABLE = [
     (7, 13)
 ]
 
+LUCAS_TABLE = [
+    (0, 2),
+    (1, 1),
+    (2, 3),
+    (3, 4),
+    (4, 7),
+    (5, 11),
+    (6, 18),
+    (7, 29),
+
+]
+
 
 @pytest.mark.parametrize('n, result', FIBB_TABLE)
 def test_fibbonacci(n, result):
     '''Test fibonacci function against defined FIBB_TABLE above.'''
     from series import fibonacci
     assert fibonacci(n) == result
+
+
+@pytest.mark.parametrize('n, result', LUCAS_TABLE)
+def test_lucas(n, result):
+    '''Test lucas function against defined LUCAS_TABLE above'''
+    from series import lucas
+    assert lucas(n) == result
