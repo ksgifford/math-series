0f9541e8873af3124f5a93cafa2048a83c301023
Kevin Gifford <ksgifford@gmail.com>
2016-03-01 14:47:13 -0800

    Updated sum_series to correct minor formatting errors and added Help message to print on run.

--- src/series.py
+++ src/series.py
@@ -26,3 +26,20 @@ def sum_series(n, val1=0, val2=1):
         return val2
     elif n > 1:
         return (sum_series(n-1, val1, val2) + sum_series(n-2, val1, val2))
+
+
+if __name__ == "__main__":
+    print (u'''
+    This module defines functions for generating Fibonacci
+    and Lucas series, as well as a custom function for applying the Fibonacci
+    formula to any two starting values.
+
+    fibonacci(n):
+        Returns the nth value of the Fibonacci series
+
+    lucas(n):
+        Returns the nth value of the Lucas series
+
+    sum_series(n, val1, val2):
+        Returns the nth value of the custom series, using val1 and val2 as
+        the first and second values of the series, respectively.''')
